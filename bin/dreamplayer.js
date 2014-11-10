5eb8d34e1ed37863563267e16aa3d796074e9862
Dimitri Nicolas <dimitri.nicolas.cn@gmail.com>
2014-11-10 22:13:19 +0100

    Improve play pause animation :bowtie:

--- bin/dreamplayer.js
+++ bin/dreamplayer.js
@@ -650,32 +650,36 @@ DreamPlayer.prototype.setPlayPause = function() {
 
 		player.addClass("playing");
 
-		player.elements["playPauseSVG1"].innerHTML = "";
-		player.elements["playPauseSVG2"].innerHTML = "";
+		if (player.elements["playPauseSVG"]) {
 
-		var playPauseSVG1animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-		playPauseSVG1animate.setAttribute("attributeType", "XML");
-		playPauseSVG1animate.setAttribute("attributeName", "points");
-		playPauseSVG1animate.setAttribute("from", player.elements["playPauseSVG1"].getAttribute("points"));
-		playPauseSVG1animate.setAttribute("to", "6,4 8,4 8,28 6,28");
-		playPauseSVG1animate.setAttribute("begin", "indefinite");
-		playPauseSVG1animate.setAttribute("dur", ".15s");
-		player.elements["playPauseSVG1"].setAttribute("points", "6,4 8,4 8,28 6,28");
+			player.elements["playPauseSVG1"].innerHTML = "";
+			player.elements["playPauseSVG2"].innerHTML = "";
 
-		var playPauseSVG2animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-		playPauseSVG2animate.setAttribute("attributeType", "XML");
-		playPauseSVG2animate.setAttribute("attributeName", "points");
-		playPauseSVG2animate.setAttribute("from", player.elements["playPauseSVG2"].getAttribute("points"));
-		playPauseSVG2animate.setAttribute("to", "24,4 26,4 26,28 24,28");
-		playPauseSVG2animate.setAttribute("begin", "indefinite");
-		playPauseSVG2animate.setAttribute("dur", ".15s");
-		player.elements["playPauseSVG2"].setAttribute("points", "24,4 26,4 26,28 24,28");
+			var playPauseSVG1animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+			playPauseSVG1animate.setAttribute("attributeType", "XML");
+			playPauseSVG1animate.setAttribute("attributeName", "points");
+			playPauseSVG1animate.setAttribute("from", player.elements["playPauseSVG1"].getAttribute("points"));
+			playPauseSVG1animate.setAttribute("to", "26,4 26,28 24,28 24,4");
+			playPauseSVG1animate.setAttribute("begin", "indefinite");
+			playPauseSVG1animate.setAttribute("dur", ".18s");
+			player.elements["playPauseSVG1"].setAttribute("points", "6,4 8,4 8,28 6,28");
 
-		player.elements["playPauseSVG1"].appendChild(playPauseSVG1animate);
-		player.elements["playPauseSVG2"].appendChild(playPauseSVG2animate);
+			var playPauseSVG2animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+			playPauseSVG2animate.setAttribute("attributeType", "XML");
+			playPauseSVG2animate.setAttribute("attributeName", "points");
+			playPauseSVG2animate.setAttribute("from", player.elements["playPauseSVG2"].getAttribute("points"));
+			playPauseSVG2animate.setAttribute("to", "6,4 8,4 8,28 6,28");
+			playPauseSVG2animate.setAttribute("begin", "indefinite");
+			playPauseSVG2animate.setAttribute("dur", ".18s");
+			player.elements["playPauseSVG2"].setAttribute("points", "24,4 26,4 26,28 24,28");
 
-		playPauseSVG1animate.beginElement();
-		playPauseSVG2animate.beginElement();
+			player.elements["playPauseSVG1"].appendChild(playPauseSVG1animate);
+			player.elements["playPauseSVG2"].appendChild(playPauseSVG2animate);
+
+			playPauseSVG1animate.beginElement();
+			playPauseSVG2animate.beginElement();
+
+		}
 
 	});
 
@@ -683,32 +687,36 @@ DreamPlayer.prototype.setPlayPause = function() {
 
 		player.removeClass("playing");
 
-		player.elements["playPauseSVG1"].innerHTML = "";
-		player.elements["playPauseSVG2"].innerHTML = "";
+		if (player.elements["playPauseSVG"]) {
 
-		var playPauseSVG1animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-		playPauseSVG1animate.setAttribute("attributeType", "XML");
-		playPauseSVG1animate.setAttribute("attributeName", "points");
-		playPauseSVG1animate.setAttribute("from", player.elements["playPauseSVG1"].getAttribute("points"));
-		playPauseSVG1animate.setAttribute("to", "6,4 28,16 6,16 6,16");
-		playPauseSVG1animate.setAttribute("begin", "indefinite");
-		playPauseSVG1animate.setAttribute("dur", ".15s");
-		player.elements["playPauseSVG1"].setAttribute("points", "6,4 28,16 6,16 6,16");
+			player.elements["playPauseSVG1"].innerHTML = "";
+			player.elements["playPauseSVG2"].innerHTML = "";
+	
+			var playPauseSVG1animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+			playPauseSVG1animate.setAttribute("attributeType", "XML");
+			playPauseSVG1animate.setAttribute("attributeName", "points");
+			playPauseSVG1animate.setAttribute("from", player.elements["playPauseSVG1"].getAttribute("points"));
+			playPauseSVG1animate.setAttribute("to", "6,4 28,16 6,16 6,16");
+			playPauseSVG1animate.setAttribute("begin", "indefinite");
+			playPauseSVG1animate.setAttribute("dur", ".18s");
+			player.elements["playPauseSVG1"].setAttribute("points", "6,4 28,16 6,16 6,16");
+	
+			var playPauseSVG2animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
+			playPauseSVG2animate.setAttribute("attributeType", "XML");
+			playPauseSVG2animate.setAttribute("attributeName", "points");
+			playPauseSVG2animate.setAttribute("from", player.elements["playPauseSVG2"].getAttribute("points"));
+			playPauseSVG2animate.setAttribute("to", "6,16 28,16 6,28 6,28");
+			playPauseSVG2animate.setAttribute("begin", "indefinite");
+			playPauseSVG2animate.setAttribute("dur", ".18s");
+			player.elements["playPauseSVG2"].setAttribute("points", "6,16 28,16 6,28 6,28");
+	
+			player.elements["playPauseSVG1"].appendChild(playPauseSVG1animate);
+			player.elements["playPauseSVG2"].appendChild(playPauseSVG2animate);
+	
+			playPauseSVG1animate.beginElement();
+			playPauseSVG2animate.beginElement();
 
-		var playPauseSVG2animate = document.createElementNS("http://www.w3.org/2000/svg", "animate");
-		playPauseSVG2animate.setAttribute("attributeType", "XML");
-		playPauseSVG2animate.setAttribute("attributeName", "points");
-		playPauseSVG2animate.setAttribute("from", player.elements["playPauseSVG2"].getAttribute("points"));
-		playPauseSVG2animate.setAttribute("to", "6,16 28,16 6,28 6,28");
-		playPauseSVG2animate.setAttribute("begin", "indefinite");
-		playPauseSVG2animate.setAttribute("dur", ".15s");
-		player.elements["playPauseSVG2"].setAttribute("points", "6,16 28,16 6,28 6,28");
-
-		player.elements["playPauseSVG1"].appendChild(playPauseSVG1animate);
-		player.elements["playPauseSVG2"].appendChild(playPauseSVG2animate);
-
-		playPauseSVG1animate.beginElement();
-		playPauseSVG2animate.beginElement();
+		}
 
 	});
 	
